79c690f8a5190ecb8890f62e9d0726bcab8664bc
r3vn <giuseppe.corti@protonmail.com>
2021-09-18 22:51:52 +0200

    [waydroid-images-api-29-bin] tmp: fix vendor download for amd64

--- getlatest.py
+++ getlatest.py
@@ -17,7 +17,11 @@ if (img == "system"):
         print(last["url"])
 
 elif (img == "vendor"):
-    halium_ver = sys.argv[3]
+
+    if (arch == 'x86_64'):
+        halium_ver = "MAINLINE"
+    else:
+        halium_ver = sys.argv[3]
 
     with open('waydroid_OTA/vendor/waydroid_%s/%s.json' % (arch, halium_ver)) as json_file:
         data = json.load(json_file)
