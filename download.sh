9a313098fd7a5c52fda094a9af348d534ffd7035
Eugenio Paolantonio (g7) <me@medesimo.eu>
2022-11-08 01:27:09 +0100

    [waydroid-images-bin] Pick up halium11 images as well
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- download.sh
+++ download.sh
@@ -33,18 +33,22 @@ fi
 SYSTEM_DOWNLOAD_URL=`python3 getlatest.py system ${ARCH}`
 VENDOR9_DOWNLOAD_URL=`python3 getlatest.py vendor ${ARCH} HALIUM_9`
 VENDOR10_DOWNLOAD_URL=`python3 getlatest.py vendor ${ARCH} HALIUM_10`
+VENDOR11_DOWNLOAD_URL=`python3 getlatest.py vendor ${ARCH} HALIUM_11`
 
 rm -rf downloaded_artifacts
 mkdir -p downloaded_artifacts/halium9
 mkdir -p downloaded_artifacts/halium10
+mkdir -p downloaded_artifacts/halium11
 mkdir -p downloaded_artifacts/system
 
 wget "${SYSTEM_DOWNLOAD_URL}" -O system.zip
 wget "${VENDOR9_DOWNLOAD_URL}" -O vendor9.zip
 wget "${VENDOR10_DOWNLOAD_URL}" -O vendor10.zip
+wget "${VENDOR11_DOWNLOAD_URL}" -O vendor11.zip
 
 unzip system.zip -d "${PWD}/downloaded_artifacts/system"
 unzip vendor9.zip -d "${PWD}/downloaded_artifacts/halium9"
 unzip vendor10.zip -d "${PWD}/downloaded_artifacts/halium10"
+unzip vendor11.zip -d "${PWD}/downloaded_artifacts/halium11"
 
-rm system.zip vendor9.zip vendor10.zip
+rm system.zip vendor9.zip vendor10.zip vendor11.zip
