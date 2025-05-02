463dfc782218e3815b00ec4b6d51a766319b39a0
Eugenio Paolantonio (g7) <me@medesimo.eu>
2025-05-03 01:30:23 +0200

    [waydroid-images-bin] Add api33 vendor images
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- download.sh
+++ download.sh
@@ -34,21 +34,25 @@ SYSTEM_DOWNLOAD_URL=`python3 getlatest.py system ${ARCH}`
 VENDOR9_DOWNLOAD_URL=`python3 getlatest.py vendor ${ARCH} HALIUM_9`
 VENDOR10_DOWNLOAD_URL=`python3 getlatest.py vendor ${ARCH} HALIUM_10`
 VENDOR11_DOWNLOAD_URL=`python3 getlatest.py vendor ${ARCH} HALIUM_11`
+VENDOR13_DOWNLOAD_URL=`python3 getlatest.py vendor ${ARCH} HALIUM_13`
 
 rm -rf downloaded_artifacts
 mkdir -p downloaded_artifacts/halium9
 mkdir -p downloaded_artifacts/halium10
 mkdir -p downloaded_artifacts/halium11
+mkdir -p downloaded_artifacts/halium13
 mkdir -p downloaded_artifacts/system
 
 wget "${SYSTEM_DOWNLOAD_URL}" -O system.zip
 wget "${VENDOR9_DOWNLOAD_URL}" -O vendor9.zip
 wget "${VENDOR10_DOWNLOAD_URL}" -O vendor10.zip
 wget "${VENDOR11_DOWNLOAD_URL}" -O vendor11.zip
+wget "${VENDOR13_DOWNLOAD_URL}" -O vendor13.zip
 
 unzip system.zip -d "${PWD}/downloaded_artifacts/system"
 unzip vendor9.zip -d "${PWD}/downloaded_artifacts/halium9"
 unzip vendor10.zip -d "${PWD}/downloaded_artifacts/halium10"
 unzip vendor11.zip -d "${PWD}/downloaded_artifacts/halium11"
+unzip vendor13.zip -d "${PWD}/downloaded_artifacts/halium13"
 
-rm system.zip vendor9.zip vendor10.zip vendor11.zip
+rm system.zip vendor9.zip vendor10.zip vendor11.zip vendor13.zip
