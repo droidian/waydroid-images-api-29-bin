daff75c4c96883e831c55b23930de4f087af934c
Eugenio Paolantonio (g7) <me@medesimo.eu>
2025-05-03 01:56:31 +0200

    [waydroid-images-bin] Allow checking on a different OTA directory
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- getlatest.py
+++ getlatest.py
@@ -1,13 +1,16 @@
 #!/user/bin/env python3
 import json
+import os
 import sys
 
 img = sys.argv[1]
 arch = sys.argv[2]
 
+basedir = "waydroid_OTA_override" if os.path.exists("waydroid_OTA_override") else "waydroid_OTA"
+
 if (img == "system"):
 
-    with open('waydroid_OTA/system/lineage/waydroid_%s/VANILLA.json' % arch) as json_file:
+    with open('%s/system/lineage/waydroid_%s/VANILLA.json' % (basedir, arch)) as json_file:
         data = json.load(json_file)
 
         for p in data['response']:
@@ -23,7 +26,7 @@ elif (img == "vendor"):
     else:
         halium_ver = sys.argv[3]
 
-    with open('waydroid_OTA/vendor/waydroid_%s/%s.json' % (arch, halium_ver)) as json_file:
+    with open('%s/vendor/waydroid_%s/%s.json' % (basedir, arch, halium_ver)) as json_file:
         data = json.load(json_file)
 
         for p in data['response']:
